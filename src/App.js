d6da7d2ac5eb8aa8910c2c0bf804d13548ea510b
Branyer <branyervergarac@gmail.com>
2020-07-25 17:16:35 -0400

    update app css and js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+
 import Header from './Header.js'
+import FormInputMovie from './FormInputMovie'
+import ListMovieResults from './ListMovieResults'
 
 import './App.css'
 import './Header.css'
+import './FormInputMovie.css'
+import './ListMovieResults.css'
 
-function App() {
+const API_KEY = '715d100a';
+const API = 'https://www.omdbapi.com/';
+
+const App = () => {
+
+    const [data, setData] = useState({});
+
+    const [values, setValues] = useState({title: '', page: 1});
+
+    const handleTitleSearch = (v) =>  {
+        setValues({title: v, page: 1});
+       
+    };
+    const handlePage = (p) => setValues({...values, page: p });
+   
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            
+            try {
+                const url=`${API}?s=${values.title}&apikey=${API_KEY}&type=movie&page=${values.page}`;
+                const response = await fetch(url);
+                const dataJson = await response.json();
+                setData(dataJson);
+    
+            } catch(e) {
+                console.log(e);
+            }
+        }
+        fetchData();
+    }, [values]);
+
     return (
         <>
          <Header />
+         <FormInputMovie  handleTitleSearch = {handleTitleSearch} />
+         <ListMovieResults  data={ data } handlePage={handlePage} page={values.page}/>
 
-         {/*Form input Searcher*/}
-         {/* How many results pages*/}
-         
-         {/* Results from Search */}
+        {/*Footer*/}
 
         </>
     )
