04969c39e012ea03b828baac72ceca412fde2708
Branyer <branyervergarac@gmail.com>
2020-07-27 11:28:35 -0400

    changing image logo

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -3,7 +3,8 @@ import React from 'react'
 function Logo({ src }) {
     return (
         <div className="header__logo">
-            <img src={ src } alt="OMDB-API logo"/>
+            {/* <img src={ src } alt="OMDB-API logo"/> */}
+            OMDB 
         </div>
     )
 }
